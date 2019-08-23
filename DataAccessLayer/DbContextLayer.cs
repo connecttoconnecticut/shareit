ce345ad548a9a533a471b5016b1c5f5e43b53715
connecttoconnecticut <boban96minic@gmail.com>
2019-08-24 00:26:06 +0200

    Entity Framework 6.2 installation

---
+++ DataAccessLayer/DbContextLayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TimeTracker.DataAccessLayer
+{
+    //Entity Framework
+    public class DbContextLayer : DbContext
+    {
+    }
+}
