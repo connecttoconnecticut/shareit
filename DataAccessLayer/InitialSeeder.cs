a8782a85caf7ec0467f3aebeeebc1dec88e358ad
connecttoconnecticut <boban96minic@gmail.com>
2019-08-24 01:08:24 +0200

    Pre seed implementation

---
+++ DataAccessLayer/InitialSeeder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeTracker.DataAccessLayer
+{
+    public class InitialSeeder : System.Data.Entity.DropCreateDatabaseIfModelChanges<TimeTrackerDBContext>
+    {
+        protected override void Seed(TimeTrackerDBContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
---
+++ DataAccessLayer/TimeTrackerDBContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using TimeTracker.Models.App;
+using TimeTracker.Models.User;
+
+namespace TimeTracker.DataAccessLayer
+{
+    public class TimeTrackerDBContext : DbContext
+    {
+        //connection string
+        public TimeTrackerDBContext() : base("TimeTrackerDBContext")
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TimeRecord> TimeRecords { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
